30f5643f4860c7eb1cbd4d97c14fdb0c6487ea6d
plnine <pl9-fund@pm.me>
2023-01-19 12:47:46 +0300

    update

---
+++ x-l1bra/nodes/celestia/install.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+#X-l1bra  
+clear && source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/scripts/common.sh)
+printLogo
+printRed  =======================================================================
+echo Установка
---
+++ x-l1bra/nodes/celestia/main.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+#X-l1bra  
+clear && source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/scripts/common.sh)
+printLogo
+printRed  =======================================================================
